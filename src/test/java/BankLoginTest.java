502b6983f8a01d155bde03800dcd5bfdb6b66af7
Ksenia Poshekhova <kposehova@gmail.com>
2024-04-23 21:10:10 +0300

    Inichial commit

---
+++ src/test/java/BankLoginTest.java
@@ -0,0 +1,68 @@
+import com.codeborne.selenide.Configuration;
+import data.DataHelper;
+import data.SQLHelper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import page.LoginPage;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.SQLHelper.cleanAuthCodes;
+import static data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown(){
+        cleanAuthCodes();
+    }
+    @AfterEach
+    static void tearDownAll(){
+        cleanDatabase();
+    }
+    @BeforeEach
+    void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        Map<String, Object> prefs = new HashMap<String , Object>();
+        prefs.put("credentials_enabled_service", false);
+        prefs.put("password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+        Configuration.browserCapabilities = options;
+
+        loginPage = open ( "http://localhost:9999", LoginPage.class);
+    }
+    @Test
+    @DisplayName("Successful login with existing username and password")
+    void shouldSuccessfulLogin () {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity() ;
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerity(verificationCode.getCode());
+    }
+    @Test
+    @DisplayName("An error message appears if the user does not exist in the database")
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! Неверно указан логин или пароль");
+    }
+    @Test
+    @DisplayName("Error notification if the login is with an active user existing in the database and a random verification code")
+    void shouldGetErrorNotificationIfFLoginWithExistUserAndRandomVerificationCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! Неверно указан логин или пароль");
+    }
+}
---
+++ src/test/java/page/VerificationPage.java
@@ -0,0 +1,31 @@
+package page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import java.net.HttpCookie;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;В
+
+public class VerificationPage {
+    private final SelenideElement code = $ ("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $ ("[data-test-id=action-verify]");
+    private final SelenideElement errorNotification = $ ("[data-test-id='error-notification'] .notification__content");
+
+    public void verifyVerificationPageVisiblity() {
+        codeField.shouldBe(visible);
+    }
+    public void verifyErrorNotification(String expectedText) {
+        errorNotification.shouldHave(exactText(expectedText)).shouldBe(visible);
+    }
+    public DashboardPage validVerity (String verificationCode) {
+        verify (verificationCode);
+        return new DashboardPage();
+    }
+    public void verify(String verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+    }
+}
